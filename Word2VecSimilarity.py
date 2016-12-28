8fb980adae90274b2b5ce0eea8b50ddf0d447fbd
rriva002 <rriva002@ucr.edu>
2016-12-28 10:09:35 -0800

    Added word tokenizer to Word2Vec similarity classifier

--- Word2VecSimilarity.py
+++ Word2VecSimilarity.py
@@ -1,4 +1,6 @@
+import nltk.data
 from numpy import array, dot
+from nltk import word_tokenize
 from gensim.matutils import unitvec
 from gensim.models.word2vec import LineSentence
 from gensim.models.word2vec import Word2Vec
@@ -12,8 +14,10 @@ class Word2VecSimilarity(AbstractTextClassifier):
     unlableled_data - Path to a file containing unlabeled data
     """
     def __init__(self, unlabeled_data):
+        self.tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
         self.unlabeled_data = unlabeled_data
         self.word2vec = Word2Vec()
+        self.training_weights = {}
         self.training_data = {}
 
     def train(self, data):
@@ -24,7 +28,7 @@ class Word2VecSimilarity(AbstractTextClassifier):
         for instance in data:
             words = set()
 
-            for word in instance.text.split(" "):
+            for word in word_tokenize(instance.text):
                 if word in self.word2vec.vocab:
                     words.add(word)
 
@@ -32,7 +36,6 @@ class Word2VecSimilarity(AbstractTextClassifier):
                 self.training_data[instance.class_value] = []
 
             if len(words) > 0:
-                # self.training_data[instance.class_value].append(words)
                 v = [self.word2vec[word] for word in words]
                 v = unitvec(array(v).mean(axis=0))
 
@@ -42,7 +45,7 @@ class Word2VecSimilarity(AbstractTextClassifier):
         words = set()
         distribution = {}
 
-        for word in instance.text.split(" "):
+        for word in word_tokenize(instance.text):
             if word in self.word2vec.vocab:
                 words.add(word)
 
@@ -63,60 +66,5 @@ class Word2VecSimilarity(AbstractTextClassifier):
                     best_score = score
 
             distribution[class_value] = max(0, best_score)
-#        text = set(instance.text.split(" "))
-#        distribution = {}
-#
-#        for class_value in self.training_data.keys():
-#            best_overall_score = 0
-#
-#            for training_instance in self.training_data[class_value]:
-#                score_a = 0
-#                score_b = 0
-#
-#                for word in text:
-#                    if word not in self.word2vec.vocab:
-#                        continue
-#
-#                    best_score = 0
-#
-#                    for training_word in training_instance:
-#                        score = self.word2vec.similarity(word, training_word)
-#
-#                        if score > best_score:
-#                            best_score = score
-#
-#                        if best_score >= 1:
-#                            break
-#
-#                    score_a += best_score
-#
-#                for training_word in training_instance:
-#                    best_score = 0
-#
-#                    for word in text:
-#                        if word not in self.word2vec.vocab:
-#                            continue
-#
-#                        score = self.word2vec.similarity(word, training_word)
-#
-#                        if score > best_score:
-#                            best_score = score
-#
-#                        if best_score >= 1:
-#                            break
-#
-#                    score_b += best_score
-#
-#                score_a /= float(len(text))
-#                score_b /= float(len(training_instance))
-#                overall_score = score_a + score_b
-#
-#                if overall_score > best_overall_score:
-#                    best_overall_score = overall_score
-#
-#                if best_overall_score >= 2:
-#                    break
-#
-#            distribution[class_value] = max(0, best_overall_score)
 
         return self._normalize_distribution(distribution)
