b36d029daf743b8daa00b62020c07bfcf383e949
rriva002 <rriva002@ucr.edu>
2017-02-28 20:24:08 -0800

    Fixed bugs in paraphrase dataset balancer

--- PPDBDatasetBalancer.py
+++ PPDBDatasetBalancer.py
@@ -1,18 +1,21 @@
+import collections
+import sys
 from mysql import connector
 from nltk import word_tokenize
+from random import shuffle
+from RandomForestTextClassifier import RandomForestTextClassifier
+from TextDatasetFileParser import Instance, TextDatasetFileParser
 
 
 class PPDBDatasetBalancer(object):
-    def __init__(self, scoring_feature, threshold=0, input_range=(1, 1),
-                 host, database, user, password):
+    def __init__(self, host, database, user, password, input_range=(1, 1),
+                 scoring_feature="PPDB2Score", threshold=0, verbose=False):
         self.__scoring_feature = scoring_feature
         self.__threshold = threshold
-        self.__input_range = input_range
+        self.__input_range = (max(1, input_range[0]), max(1, input_range[1]))
         self.__connection = connector.connect(host=host, database=database,
                                               user=user, password=password)
-
-        self.__input_range[0] = max(1, self.__input_range[0])
-        self.__input_range[1] = max(1, self.__input_range[1])
+        self.__verbose = verbose
 
         if self.__input_range[0] > self.__input_range[1]:
             self.__input_range = self.__input_range[::-1]
@@ -22,9 +25,24 @@ class PPDBDatasetBalancer(object):
             print("Not connected to database.")
             return
 
+        classes = []
+
+        for instance in data:
+            classes.append(instance.class_value)
+
+            instance.weight = 1
+
+        counter = collections.Counter(classes)
+        most_common = counter.most_common(1)[0][0]
         new_instances = []
 
         for instance in data:
+            if instance.class_value == most_common:
+                continue
+
+            if self.__verbose:
+                print(instance.text)
+
             class_value = instance.class_value
             words = word_tokenize(instance.text)
             num_words = len(words)
@@ -40,7 +58,7 @@ class PPDBDatasetBalancer(object):
                     phrase += (" " if length > 0 else "") + words[j]
                     length += 1
 
-                if length < input_range[0]:
+                if length < self.__input_range[0]:
                     break
 
                 self.__find_paraphrases(phrase, paraphrases)
@@ -58,17 +76,26 @@ class PPDBDatasetBalancer(object):
                 for paraphrase in paraphrases[phrase].keys():
                     paraphrased = text[0:index] + paraphrase + text[index:]
 
+                    if self.__verbose:
+                        print("    " + paraphrased)
+
                     new_instances.append(Instance(paraphrased, class_value))
 
+        if self.__verbose:
+            print("Added " + str(len(new_instances)) + " new instances.")
         return data + new_instances
 
     def close_connection(self):
         self.__connection.close()
 
     def __find_paraphrases(self, phrase, paraphrases):
-        sql = "SELECT paraphrase, PPDB2Score FROM ppdb WHERE phrase = '"
-        sql += phrase.replace("\\", "\\\\").replace("'", "\\'")
-        sql += "' AND PPDB2Score > " + str(self.__threshold)
+        if phrase in paraphrases.keys():
+            return
+
+        sql = "SELECT paraphrase, " + self.__scoring_feature + " FROM ppdb " \
+            + "WHERE phrase = '" \
+            + phrase.replace("\\", "\\\\").replace("'", "\\'") + "' AND " \
+            + self.__scoring_feature + " > " + str(self.__threshold)
         cursor = self.__connection.cursor()
 
         cursor.execute(sql)
