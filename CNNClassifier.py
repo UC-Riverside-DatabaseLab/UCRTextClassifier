820015960e99cb3697c6f37ba37592391c927722
rriva002 <rriva002@ucr.edu>
2017-03-09 18:56:57 -0800

    Fixed bugs with CNN Word2Vec code

--- CNNClassifier.py
+++ CNNClassifier.py
@@ -1,163 +1,206 @@
 #######################################################
 #######################################################
 ############## Moloud Shahbazi
-############## Convolutional Neural Network implemented by ... extended to include word2vec representations as initial embedding values.
+############## Convolutional Neural Network implemented
+############## by ... extended to include word2vec
+############## representations as initial embedding
+############## values.
 #######################################################
 #######################################################
 
 import tensorflow as tf
 import numpy as np
-import time
-import datetime
-import os, sys
+import os
+import sys
 sys.path.insert(0, os.path.abspath("./cnn"))
 import data_helpers as data_helpers
 from text_cnn import TextCNN
 from tensorflow.contrib import learn
 from AbstractTextClassifier import AbstractTextClassifier
+from TextDatasetFileParser import TextDatasetFileParser
+from gensim.models.word2vec import Word2Vec
+
+
+def trainWord2Vec(unlabeled_data):
+    unlabeled = TextDatasetFileParser().parse_unlabeled(unlabeled_data)
+    word2vec = Word2Vec()
+    dictionary = {}
+
+    word2vec.build_vocab(unlabeled)
+    word2vec.train(unlabeled)
+
+    for word in word2vec.vocab:
+        dictionary[word] = word2vec[word]
+
+    return dictionary
+
 
 def loadWord2Vec(word2vecAddress):
-	word2vec = {}
-	with open(word2vecAddress, 'r') as f:
-		head = f.readline()
-		for line in f:
-			wv = line.strip().split(' ')
-			word2vec[wv[0]] = [float(x) for x in wv[1:]]
-	return word2vec
+    word2vec = {}
+    with open(word2vecAddress, 'r') as f:
+        f.readline()
+        for line in f:
+            wv = line.strip().split(' ')
+            word2vec[wv[0]] = [float(x) for x in wv[1:]]
+    return word2vec
+
 
 class CNNClassifier(AbstractTextClassifier):
-	def __init__(self, embedding_dim=128,filter_sizes="2,3,4,5",num_filters=128,dropout_keep_prob=0.5,l2_reg_lambda=0.0,batch_size=64,num_epochs=20,evaluate_every=100,checkpoint_every=100,allow_soft_placement=True,log_device_placement=False, word2vecAddress=None):
-		self.sess = None
-		vocab_processor = None
-		self.cnn = None
-		# Parameters
-		# ==================================================
-		# Model Hyperparameters
-		tf.flags.DEFINE_integer("embedding_dim", embedding_dim, "Dimensionality of character embedding (default: 128)")
-		tf.flags.DEFINE_string("filter_sizes", filter_sizes, "Comma-separated filter sizes (default: '3,4,5')")
-		tf.flags.DEFINE_integer("num_filters", num_filters, "Number of filters per filter size (default: 128)")
-		tf.flags.DEFINE_float("dropout_keep_prob", dropout_keep_prob, "Dropout keep probability (default: 0.5)")
-		tf.flags.DEFINE_float("l2_reg_lambda", l2_reg_lambda, "L2 regularizaion lambda (default: 0.0)")
+    def __init__(self, embedding_dim=128, filter_sizes="2,3,4,5",
+                 num_filters=128, dropout_keep_prob=0.5, l2_reg_lambda=0.0,
+                 batch_size=64, num_epochs=20, evaluate_every=100,
+                 checkpoint_every=100, allow_soft_placement=True,
+                 log_device_placement=False, word2vecAddress=None):
+        self.sess = None
+        self.cnn = None
+        # Parameters
+        # ==================================================
+        # Model Hyperparameters
+        tf.flags.DEFINE_integer("embedding_dim", embedding_dim,
+                                "Dimensionality of character embedding "
+                                "(default: 128)")
+        tf.flags.DEFINE_string("filter_sizes", filter_sizes,
+                               "Comma-separated filter sizes "
+                               "(default: '3,4,5')")
+        tf.flags.DEFINE_integer("num_filters", num_filters,
+                                "Number of filters per filter size "
+                                "(default: 128)")
+        tf.flags.DEFINE_float("dropout_keep_prob", dropout_keep_prob,
+                              "Dropout keep probability (default: 0.5)")
+        tf.flags.DEFINE_float("l2_reg_lambda", l2_reg_lambda,
+                              "L2 regularizaion lambda (default: 0.0)")
 
-		# Training parameters
-		tf.flags.DEFINE_integer("batch_size", batch_size, "Batch Size (default: 64)")
-		#tf.flags.DEFINE_integer("num_epochs", 20000, "Number of training epochs (default: 200)")
-		tf.flags.DEFINE_integer("num_epochs", num_epochs, "Number of training epochs (default: 200)")
-		tf.flags.DEFINE_integer("evaluate_every", evaluate_every, "Evaluate model on dev set after this many steps (default: 100)")
-		tf.flags.DEFINE_integer("checkpoint_every", checkpoint_every, "Save model after this many steps (default: 100)")
-		# Misc Parameters
-		tf.flags.DEFINE_boolean("allow_soft_placement", allow_soft_placement, "Allow device soft device placement")
-		tf.flags.DEFINE_boolean("log_device_placement", log_device_placement, "Log placement of ops on devices")
-		self.class_names = []
-		self.w2v = None
-		if word2vecAddress != None:
-			self.w2v = loadWord2Vec(word2vecAddress)		
+        # Training parameters
+        tf.flags.DEFINE_integer("batch_size", batch_size,
+                                "Batch Size (default: 64)")
+        # tf.flags.DEFINE_integer("num_epochs", 20000,
+        #                         "Number of training epochs (default: 200)")
+        tf.flags.DEFINE_integer("num_epochs", num_epochs,
+                                "Number of training epochs (default: 200)")
+        tf.flags.DEFINE_integer("evaluate_every", evaluate_every,
+                                "Evaluate model on dev set after this many "
+                                "steps (default: 100)")
+        tf.flags.DEFINE_integer("checkpoint_every", checkpoint_every,
+                                "Save model after this many steps "
+                                "(default: 100)")
+        # Misc Parameters
+        tf.flags.DEFINE_boolean("allow_soft_placement", allow_soft_placement,
+                                "Allow device soft device placement")
+        tf.flags.DEFINE_boolean("log_device_placement", log_device_placement,
+                                "Log placement of ops on devices")
+        self.class_names = []
+        self.w2v = None if word2vecAddress is None else \
+            trainWord2Vec(word2vecAddress)
+            # loadWord2Vec(word2vecAddress)
 
-	def train(self, data):
-		FLAGS = tf.flags.FLAGS
-		FLAGS._parse_flags()
-		# Data Preparatopn
-		# ==================================================
-		#Load Data
-		labels = set()
-		for i in data:
-			labels.add(i.class_value)
-		self.class_names = list(labels)
-		if self.w2v == None:
-			x_text, y = data_helpers.load_data_and_labels_from_instances(data, self.class_names)
-		else:
-			x_text, y = data_helpers.load_data_and_labels_from_instances_withWord2vec(data, self.class_names, self.w2v)
-			
+    def train(self, data):
+        FLAGS = tf.flags.FLAGS
+        FLAGS._parse_flags()
+        # Data Preparatopn
+        # ==================================================
+        # Load Data
+        labels = set()
+        for i in data:
+            labels.add(i.class_value)
+        self.class_names = list(labels)
+        if self.w2v is None:
+            x_text, y = data_helpers.load_data_and_labels_from_instances(data, self.class_names)
+        else:
+            x_text, y = data_helpers.load_data_and_labels_from_instances_withWord2vec(data, self.class_names, self.w2v)
 
-		# Build vocabulary
-		max_document_length = max([len(x.split(" ")) for x in x_text])
-		self.vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
-		x = np.array(list(self.vocab_processor.fit_transform(x_text)))
+        # Build vocabulary
+        max_document_length = max([len(x.split(" ")) for x in x_text])
+        self.vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
+        x = np.array(list(self.vocab_processor.fit_transform(x_text)))
 
-		# Randomly shuffle data
-		np.random.seed(10)
-		shuffle_indices = np.random.permutation(np.arange(len(y)))
-		x_shuffled = x[shuffle_indices]
-		y_shuffled = y[shuffle_indices]
+        # Randomly shuffle data
+        np.random.seed(10)
+        shuffle_indices = np.random.permutation(np.arange(len(y)))
+        x_shuffled = x[shuffle_indices]
+        y_shuffled = y[shuffle_indices]
 
-		x_train = x_shuffled
-		y_train = y_shuffled
+        x_train = x_shuffled
+        y_train = y_shuffled
 
-		# Training
-		# ==================================================
-		with tf.Graph().as_default():
-			session_conf = tf.ConfigProto(
-			  allow_soft_placement=FLAGS.allow_soft_placement,
-			  log_device_placement=FLAGS.log_device_placement)
-			self.sess = tf.Session(config=session_conf)
-			with self.sess.as_default():
-				self.cnn = TextCNN(
-					sequence_length=x_train.shape[1],
-					num_classes=y_train.shape[1],
-					vocab_size=len(self.vocab_processor.vocabulary_),
-					embedding_size=FLAGS.embedding_dim,
-					filter_sizes=list(map(int, FLAGS.filter_sizes.split(","))),
-					num_filters=FLAGS.num_filters,
-					l2_reg_lambda=FLAGS.l2_reg_lambda,
-					word2vec=self.w2v,
-					relu1=False,
-					relu2=False)
+        # Training
+        # ==================================================
+        with tf.Graph().as_default():
+            session_conf = tf.ConfigProto(
+              allow_soft_placement=FLAGS.allow_soft_placement,
+              log_device_placement=FLAGS.log_device_placement)
+            self.sess = tf.Session(config=session_conf)
+            with self.sess.as_default():
+                self.cnn = TextCNN(
+                    sequence_length=x_train.shape[1],
+                    num_classes=y_train.shape[1],
+                    vocab_size=len(self.vocab_processor.vocabulary_),
+                    embedding_size=FLAGS.embedding_dim,
+                    filter_sizes=list(map(int, FLAGS.filter_sizes.split(","))),
+                    num_filters=FLAGS.num_filters,
+                    l2_reg_lambda=FLAGS.l2_reg_lambda,
+                    word2vec=self.w2v,
+                    relu1=False,
+                    relu2=False)
 
-			# Define Training procedure
-			global_step = tf.Variable(0, name="global_step", trainable=False)
-			optimizer = tf.train.AdamOptimizer(1e-3)
-			grads_and_vars = optimizer.compute_gradients(self.cnn.loss)
-			train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
+            # Define Training procedure
+            global_step = tf.Variable(0, name="global_step", trainable=False)
+            optimizer = tf.train.AdamOptimizer(1e-3)
+            grads_and_vars = optimizer.compute_gradients(self.cnn.loss)
+            train_op = optimizer.apply_gradients(grads_and_vars,
+                                                 global_step=global_step)
 
-			# Keep track of gradient values and sparsity (optional)
-			grad_summaries = []
-			for g, v in grads_and_vars:
-				if g is not None:
-					grad_hist_summary = tf.summary.histogram("{}/grad/hist".format(v.name.replace(":", "_")), g)
-					sparsity_summary = tf.summary.scalar("{}/grad/sparsity".format(v.name.replace(":", "_")), tf.nn.zero_fraction(g))
-					grad_summaries.append(grad_hist_summary)
-					grad_summaries.append(sparsity_summary)
-			grad_summaries_merged = tf.summary.merge(grad_summaries)
+            # Keep track of gradient values and sparsity (optional)
+            grad_summaries = []
+            for g, v in grads_and_vars:
+                if g is not None:
+                    grad_hist_summary = tf.summary.histogram("{}/grad/hist".format(v.name.replace(":", "_")), g)
+                    sparsity_summary = tf.summary.scalar("{}/grad/sparsity".format(v.name.replace(":", "_")), tf.nn.zero_fraction(g))
+                    grad_summaries.append(grad_hist_summary)
+                    grad_summaries.append(sparsity_summary)
+            tf.summary.merge(grad_summaries)
 
-			# Initialize all variables
-			self.sess.run(tf.global_variables_initializer())
+            # Initialize all variables
+            self.sess.run(tf.global_variables_initializer())
 
-			def train_step(x_batch, y_batch):
-				"""
-				A single training step
-				"""
-				feed_dict = {
-				  self.cnn.input_x: x_batch,
-				  self.cnn.input_y: y_batch,
-				  self.cnn.dropout_keep_prob: FLAGS.dropout_keep_prob
-				}
-				_, step, loss, accuracy = self.sess.run(
-					[train_op, global_step, self.cnn.loss, self.cnn.accuracy],
-					feed_dict)
-			# Generate batches
-			batches = data_helpers.batch_iter(
-				list(zip(x_train, y_train)), FLAGS.batch_size, FLAGS.num_epochs)
-			# Training loop. For each batch...
-			for batch in batches:
-				x_batch, y_batch = zip(*batch)
-				train_step(x_batch, y_batch)
-				current_step = tf.train.global_step(self.sess, global_step)
-			
-	def classify(self, instance):
-		"""
-		Evaluates model on a dev set
-		"""
-		x_text, y = data_helpers.load_data_and_labels_from_instances([instance], self.class_names)
-		x = np.array(list(self.vocab_processor.fit_transform(x_text)))
-		# Build vocabulary
-		feed_dict = {
-		  self.cnn.input_x: x,
-		  self.cnn.input_y: y,
-		  self.cnn.dropout_keep_prob: 1.0
-		}
-		scores, distributions = self.sess.run([self.cnn.scores, self.cnn.distributions], feed_dict)
-		#print(scores,distributions)
-		distribution = {}
-		for i in range(len(distributions[0])):
-			distribution[self.class_names[i]] = distributions[0][i]
-		return distribution
+            def train_step(x_batch, y_batch):
+                """
+                A single training step
+                """
+                feed_dict = {
+                  self.cnn.input_x: x_batch,
+                  self.cnn.input_y: y_batch,
+                  self.cnn.dropout_keep_prob: FLAGS.dropout_keep_prob
+                }
+                _, step, loss, accuracy = self.sess.run(
+                    [train_op, global_step, self.cnn.loss, self.cnn.accuracy],
+                    feed_dict)
+            # Generate batches
+            batches = data_helpers.batch_iter(list(zip(x_train, y_train)),
+                                              FLAGS.batch_size,
+                                              FLAGS.num_epochs)
+            # Training loop. For each batch...
+            for batch in batches:
+                x_batch, y_batch = zip(*batch)
+                train_step(x_batch, y_batch)
+                tf.train.global_step(self.sess, global_step)
+
+    def classify(self, instance):
+        """
+        Evaluates model on a dev set
+        """
+        x_text, y = data_helpers.load_data_and_labels_from_instances([instance], self.class_names)
+        x = np.array(list(self.vocab_processor.fit_transform(x_text)))
+        # Build vocabulary
+        feed_dict = {
+          self.cnn.input_x: x,
+          self.cnn.input_y: y,
+          self.cnn.dropout_keep_prob: 1.0
+        }
+        scores, distributions = self.sess.run([self.cnn.scores,
+                                               self.cnn.distributions],
+                                              feed_dict)
+        # print(scores,distributions)
+        distribution = {}
+        for i in range(len(distributions[0])):
+            distribution[self.class_names[i]] = distributions[0][i]
+        return distribution
--- cnn/text_cnn.py
+++ cnn/text_cnn.py
@@ -7,119 +7,131 @@
 import tensorflow as tf
 import numpy as np
 
-#Add wordEmbeddings to initialize the embedding tensor, W
-#Add Relu before fully connected
-#Add another Relu before fully connected
-#Add control over model type
+# Add wordEmbeddings to initialize the embedding tensor, W
+# Add Relu before fully connected
+# Add another Relu before fully connected
+# Add control over model type
+
 
 class TextCNN(object):
-	"""
-	A CNN for text classification.
-	Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
-	"""
-	def __init__(
-	  self, sequence_length, num_classes, vocab_size,
-	  embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0, word2vec=None, relu1=False, relu2=False):
+    """
+    A CNN for text classification. Uses an embedding layer, followed by a
+    convolutional, max-pooling and softmax layer.
+    """
+    def __init__(self, sequence_length, num_classes, vocab_size,
+                 embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0,
+                 word2vec=None, relu1=False, relu2=False):
 
-		# Placeholders for input, output and dropout
-		self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
-		self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
-		self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
+        # Placeholders for input, output and dropout
+        self.input_x = tf.placeholder(tf.int32, [None, sequence_length],
+                                      name="input_x")
+        self.input_y = tf.placeholder(tf.float32, [None, num_classes],
+                                      name="input_y")
+        self.dropout_keep_prob = tf.placeholder(tf.float32,
+                                                name="dropout_keep_prob")
 
-		# Keeping track of l2 regularization loss (optional)
-		l2_loss = tf.constant(0.0)
+        # Keeping track of l2 regularization loss (optional)
+        l2_loss = tf.constant(0.0)
 
-		# Embedding layer
-		with tf.device('/cpu:0'), tf.name_scope("embedding"):
-			if word2vec != None:
-				W = tf.Variable(
-					tf.pack(np.array([len(word2vec.values()[0]) * [0]] + word2vec.values())),
-					name="W")
-				embedding_size = len(word2vec.values()[0])
-				vocab_size = len(word2vec.keys()) + 1
-			else:
-				W = tf.Variable(
-					tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
-					name="W")
-			self.embedded_chars = tf.nn.embedding_lookup(W, self.input_x)
-			self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
+        # Embedding layer
+        with tf.device('/cpu:0'), tf.name_scope("embedding"):
+            if word2vec is not None:
+                values = []
+                for value in word2vec.values():
+                    values.append(value)
+                W = tf.Variable(tf.pack(np.array([len(values[0]) * [0]] +
+                                                 values, dtype=np.float32)),
+                                name="W")
+                embedding_size = len(values[0])
+                vocab_size = len(word2vec.keys()) + 1
+            else:
+                W = tf.Variable(tf.random_uniform([vocab_size, embedding_size],
+                                                  -1.0, 1.0),
+                                name="W")
+            self.embedded_chars = tf.nn.embedding_lookup(W, self.input_x)
+            self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars,
+                                                          -1)
 
-		# Create a convolution + maxpool layer for each filter size
-		pooled_outputs = []
-		for i, filter_size in enumerate(filter_sizes):
-			with tf.name_scope("conv-maxpool-%s" % filter_size):
-				# Convolution Layer
-				filter_shape = [filter_size, embedding_size, 1, num_filters]
-				W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
-				b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
-				conv = tf.nn.conv2d(
-					self.embedded_chars_expanded,
-					W,
-					strides=[1, 1, 1, 1],
-					padding="VALID",
-					name="conv")
-				# Apply nonlinearity
-				h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-				# Maxpooling over the outputs
-				pooled = tf.nn.max_pool(
-					h,
-					ksize=[1, sequence_length - filter_size + 1, 1, 1],
-					strides=[1, 1, 1, 1],
-					padding='VALID',
-					name="pool")
-				pooled_outputs.append(pooled)
-		# Combine all the pooled features
-		num_filters_total = num_filters * len(filter_sizes)
-		self.h_pool = tf.concat(3, pooled_outputs)
-		self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
+        # Create a convolution + maxpool layer for each filter size
+        pooled_outputs = []
+        for i, filter_size in enumerate(filter_sizes):
+            with tf.name_scope("conv-maxpool-%s" % filter_size):
+                # Convolution Layer
+                filter_shape = [filter_size, embedding_size, 1, num_filters]
+                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1),
+                                name="W")
+                b = tf.Variable(tf.constant(0.1, shape=[num_filters]),
+                                name="b")
+                conv = tf.nn.conv2d(self.embedded_chars_expanded, W,
+                                    strides=[1, 1, 1, 1], padding="VALID",
+                                    name="conv")
+                # Apply nonlinearity
+                h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+                # Maxpooling over the outputs
+                pooled = tf.nn.max_pool(h, ksize=[1, sequence_length -
+                                        filter_size + 1, 1, 1], name="pool",
+                                        strides=[1, 1, 1, 1], padding='VALID')
+                pooled_outputs.append(pooled)
+        # Combine all the pooled features
+        num_filters_total = num_filters * len(filter_sizes)
+        self.h_pool = tf.concat(3, pooled_outputs)
+        self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
 
-		# Add dropout
-		with tf.name_scope("dropout"):
-			self.h_drop = tf.nn.dropout(self.h_pool_flat, self.dropout_keep_prob)
+        # Add dropout
+        with tf.name_scope("dropout"):
+            self.h_drop = tf.nn.dropout(self.h_pool_flat,
+                                        self.dropout_keep_prob)
 
+        if relu1:
+            # Add a hidden layer
+            n_hidden1 = 128
+            with tf.name_scope("relu1"):
+                wh1 = tf.Variable(tf.random_normal([num_filters_total,
+                                                    n_hidden1]), name="Wh")
+                bh1 = tf.Variable(tf.random_normal([n_hidden1]), name="Bh")
+                relulayer_1 = tf.nn.relu(tf.nn.xw_plus_b(self.h_drop, wh1,
+                                                         bh1))
 
-		if relu1:
-			# Add a hidden layer
-			n_hidden1 = 128
-			with tf.name_scope("relu1"):
-				wh1 = tf.Variable(tf.random_normal([num_filters_total, n_hidden1]), name="Wh")
-				bh1 = tf.Variable(tf.random_normal([n_hidden1]), name="Bh")
-				relulayer_1 = tf.nn.relu(tf.nn.xw_plus_b(self.h_drop, wh1, bh1))
-		
-			if relu2:
-				# Add anothe hidden layer
-				n_hidden2 = 128
-				with tf.name_scope("relu2"):
-					wh2 = tf.Variable(tf.random_normal([n_hidden1, n_hidden2]), name="Wh")
-					bh2 = tf.Variable(tf.random_normal([n_hidden2]), name="Bh")
-					relulayer_2 = tf.nn.relu(tf.nn.xw_plus_b(relulayer_1, wh2, bh2))
-			else:
-				relulayer_2 = relulayer_1
-		else:
-			relulayer_2 = self.h_drop
+            if relu2:
+                # Add anothe hidden layer
+                n_hidden2 = 128
+                with tf.name_scope("relu2"):
+                    wh2 = tf.Variable(tf.random_normal([n_hidden1, n_hidden2]),
+                                      name="Wh")
+                    bh2 = tf.Variable(tf.random_normal([n_hidden2]), name="Bh")
+                    relulayer_2 = tf.nn.relu(tf.nn.xw_plus_b(relulayer_1, wh2,
+                                                             bh2))
+            else:
+                relulayer_2 = relulayer_1
+        else:
+            relulayer_2 = self.h_drop
 
-		print(relulayer_2.get_shape())
-		# Final (unnormalized) scores and predictions
-		with tf.name_scope("output"):
-			W = tf.get_variable(
-				"W",
-				#shape=[num_filters_total, num_classes],
-				#shape=[n_hidden2, num_classes],
-				shape=[relulayer_2.get_shape()[1], num_classes],
-				initializer=tf.contrib.layers.xavier_initializer())
-			b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
-			l2_loss += tf.nn.l2_loss(W)
-			l2_loss += tf.nn.l2_loss(b)
-			#self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
-			self.scores = tf.nn.xw_plus_b(relulayer_2, W, b, name="scores")
-			self.predictions = tf.argmax(self.scores, 1, name="predictions")
+        print(relulayer_2.get_shape())
+        # Final (unnormalized) scores and predictions
+        with tf.name_scope("output"):
+            W = tf.get_variable(
+                "W",
+                # shape=[num_filters_total, num_classes],
+                # shape=[n_hidden2, num_classes],
+                shape=[relulayer_2.get_shape()[1], num_classes],
+                initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
+            l2_loss += tf.nn.l2_loss(W)
+            l2_loss += tf.nn.l2_loss(b)
+            # self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
+            self.scores = tf.nn.xw_plus_b(relulayer_2, W, b, name="scores")
+            self.predictions = tf.argmax(self.scores, 1, name="predictions")
+            self.distributions = tf.nn.softmax(self.scores)
 
-		# CalculateMean cross-entropy loss
-		with tf.name_scope("loss"):
-			losses = tf.nn.softmax_cross_entropy_with_logits(self.scores, self.input_y)
-			self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+        # CalculateMean cross-entropy loss
+        with tf.name_scope("loss"):
+            losses = tf.nn.softmax_cross_entropy_with_logits(self.scores,
+                                                             self.input_y)
+            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
 
-		# Accuracy
-		with tf.name_scope("accuracy"):
-			correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
-			self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
+        # Accuracy
+        with tf.name_scope("accuracy"):
+            correct_predictions = tf.equal(self.predictions,
+                                           tf.argmax(self.input_y, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_predictions,
+                                                   "float"), name="accuracy")
