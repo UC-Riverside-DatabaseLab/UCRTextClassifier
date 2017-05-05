9ec381e0becc0aa980ad7baa66b62463b1eb980c
rriva002 <rriva002@ucr.edu>
2017-05-04 18:02:06 -0700

    Added information gain option to PPDB balancer
    Only phrases that contain a word with information gain above a given
    threshold are considered for paraphrase queries.

--- PPDBDatasetBalancer.py
+++ PPDBDatasetBalancer.py
@@ -1,14 +1,18 @@
 import collections
+import math
 from mysql import connector
 from nltk import word_tokenize
+from sklearn.feature_extraction.text import CountVectorizer
 from TextDatasetFileParser import Instance
 
 
 class PPDBDatasetBalancer(object):
     def __init__(self, host, database, user, password, input_range=(1, 1),
-                 scoring_feature="PPDB2Score", threshold=0, verbose=False):
+                 scoring_feature="PPDB2Score", threshold=0, ig_threshold=0,
+                 verbose=False):
         self.__scoring_feature = scoring_feature
         self.__threshold = threshold
+        self.__ig_threshold = ig_threshold
         self.__input_range = (max(1, input_range[0]), max(1, input_range[1]))
         self.__connection = connector.connect(host=host, database=database,
                                               user=user, password=password)
@@ -23,6 +27,8 @@ class PPDBDatasetBalancer(object):
             return
 
         classes = []
+        top_words = self.__top_information_gain_words(data) \
+            if self.__ig_threshold > 0 else None
 
         for instance in data:
             classes.append(instance.class_value)
@@ -58,13 +64,13 @@ class PPDBDatasetBalancer(object):
                 if length < self.__input_range[0]:
                     break
 
-                self.__find_paraphrases(phrase, paraphrases)
+                self.__find_paraphrases(phrase, paraphrases, top_words)
 
                 for j in range(i + length, max_cutoff):
                     phrase += (" " if length > 0 else "") + words[j]
                     length += 1
 
-                    self.__find_paraphrases(phrase, paraphrases)
+                    self.__find_paraphrases(phrase, paraphrases, top_words)
 
             for phrase in paraphrases.keys():
                 index = instance.text.find(phrase)
@@ -85,9 +91,36 @@ class PPDBDatasetBalancer(object):
     def close_connection(self):
         self.__connection.close()
 
-    def __find_paraphrases(self, phrase, paraphrases):
+    def __entropy(self, vectors, index):
+        value_frequencies = {}
+        entropy = 0
+
+        for vector in vectors:
+            if vector[index] not in value_frequencies:
+                value_frequencies[vector[index]] = 0
+
+            value_frequencies[vector[index]] += 1
+
+        for frequency in value_frequencies.values():
+            ratio = frequency / len(vectors)
+            entropy -= ratio * math.log(ratio, 2)
+
+        return entropy
+
+    def __find_paraphrases(self, phrase, paraphrases, top_words):
         if phrase in paraphrases.keys():
             return
+
+        if top_words is not None:
+            valid = False
+
+            for word in top_words:
+                if phrase.find(word) > -1:
+                    valid = True
+                    break
+
+            if not valid:
+                return
 
         sql = "SELECT paraphrase, " + self.__scoring_feature + " FROM ppdb " \
             + "WHERE phrase = '" \
@@ -104,3 +137,41 @@ class PPDBDatasetBalancer(object):
             paraphrases[phrase][row[0]] = row[1]
 
         cursor.close()
+
+    def __top_information_gain_words(self, data):
+        vectorizer = CountVectorizer(stop_words="english")
+        text = []
+        labels = []
+        top_words = []
+        word_frequencies = {}
+
+        for instance in data:
+            text.append(instance.text)
+            labels.append([instance.class_value])
+
+        vectors = vectorizer.fit_transform(text)
+        words = vectorizer.get_feature_names()
+        entropy = self.__entropy(labels, 0)
+
+        for i in range(0, len(words)):
+            word_frequencies[i] = {}
+
+        for vector in vectors:
+            for i in range(0, len(vector)):
+                if vector[i] not in word_frequencies[i]:
+                    word_frequencies[i][vector[i]] = 0
+
+                word_frequencies[i][vector[i]] += 1
+
+        for index, value_frequencies in word_frequencies.items():
+            subset_entropy = 0
+
+            for value, frequency in value_frequencies.items():
+                value_probability = frequency / sum(value_frequencies.values())
+                sub = [vector for vector in vectors if vector[index] == value]
+                subset_entropy += value_probability * entropy(sub, index)
+
+            if entropy - subset_entropy > self.__ig_threshold:
+                top_words.append(words[index])
+
+        return top_words
